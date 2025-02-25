5f5a5d1a2229b4939f5ff0bf7381b0fe1bc74ae7
Paurakh <pandeypaurakhraj@gmail.com>
2025-02-25 22:19:28 +0545

    dict

---
+++ Python/Data Strcutures/dict_exe.py
@@ -0,0 +1,5 @@
+from icecream import ic
+
+ic( dict([("name", "Alice"), ("age", 25), ("city", "New York")]) )
+ic(dict ({("name", "Alice"), ("age", 25), ("city", "New York")} ) ) # this is a set of tuples
+
