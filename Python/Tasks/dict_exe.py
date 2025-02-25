b9562d70ef172f4940fda400d405d93fe466d687
Paurakh <pandeypaurakhraj@gmail.com>
2025-02-25 20:55:00 +0545

    new file

---
+++ Python/Tasks/dict_exe.py
@@ -0,0 +1,10 @@
+# Group Data by a Key
+# Scenario: You have data points with categories and need to group them accordingly.
+
+# Task:
+
+# Input: [('fruit', 'apple'), ('vegetable', 'carrot'), ('fruit', 'banana'), ('vegetable', 'broccoli')]
+# Use a dictionary to group items by category.
+# Expected output: {'fruit': ['apple', 'banana'], 'vegetable': ['carrot', 'broccoli']}
+
+data= [('fruit', 'apple'), ('vegetable', 'carrot'), ('fruit', 'banana'), ('vegetable', 'broccoli')]
